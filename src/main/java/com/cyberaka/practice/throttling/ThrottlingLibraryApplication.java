c2eabdc96b3dd72ab233310fea5f594d4424754c
Abhinav Anand <cyberaka@gmail.com>
2021-11-17 19:26:56 +0530

    Initial code setup.

---
+++ src/main/java/com/cyberaka/practice/throttling/ThrottlingLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.cyberaka.practice.throttling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrottlingLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrottlingLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyberaka/practice/throttling/ThrottlingLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyberaka.practice.throttling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrottlingLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
